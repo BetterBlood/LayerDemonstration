7a49c527064c035f0ee7d79da6bfe5b1154014e9
Better_Blood <better_blood@hotmail.com>
2025-04-26 22:11:59 +0200

    icons for hierarchy

--- scripts/health_component.gd
+++ scripts/health_component.gd
@@ -1,6 +1,10 @@
+@icon("res://icons/heart_2d.svg")
 extends Node2D
 
 class_name HealthComponent
 
 func take_fire_effect() -> void:
 	print(self, ": I'm literally on fire....")
+
+func get_icons_path():
+	return "icon.svg"
